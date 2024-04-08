799a7d019bc95c3f6b3d7b9ad4048c1864fe70be
KristofferStrube <kristoffer.strube@gmail.com>
2024-04-08 21:15:48 +0200

    New integration method with My Relewise.

--- samples/KristofferStrube.Blazor.Relewise.WasmExample/StaticDatasetStorage.cs
+++ samples/KristofferStrube.Blazor.Relewise.WasmExample/StaticDatasetStorage.cs
@@ -5,5 +5,5 @@ public static class StaticDatasetStorage
     public static string ServerUrl { get; set; } = "https://api.relewise.com/";
     public static string DatasetId { get; set; } = "";
     public static string ApiKey { get; set; } = "";
-    public static bool NoMenu { get; set; } = false;
+    public static string? ParentOrigin { get; set; }
 }
