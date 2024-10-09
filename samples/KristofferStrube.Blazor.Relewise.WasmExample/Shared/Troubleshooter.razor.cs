7d6ff45b53907a5b3c4d8c0bbe89132167371285
KristofferStrube <kristoffer.strube@gmail.com>
2024-10-09 23:42:29 +0200

    Then by edits instead of the other way around.

--- samples/KristofferStrube.Blazor.Relewise.WasmExample/Shared/Troubleshooter.razor.cs
+++ samples/KristofferStrube.Blazor.Relewise.WasmExample/Shared/Troubleshooter.razor.cs
@@ -233,7 +233,7 @@ namespace KristofferStrube.Blazor.Relewise.WasmExample.Shared
                                 if (searchResults.FirstOrDefault() is { } matchCollection)
                                 {
                                     var lowestNumberOfEdits = matchCollection.Matches.Min(m => m.Edits);
-                                    var goodEnoughMatches = matchCollection.Matches.Where(m => m.Edits <= lowestNumberOfEdits + 1).OrderBy(m => m.Edits).ThenBy(m => m.Position);
+                                    var goodEnoughMatches = matchCollection.Matches.Where(m => m.Edits <= lowestNumberOfEdits + 1).OrderBy(m => m.Position).ThenBy(m => m.Edits);
                                     if (goodEnoughMatches.Count() > 0)
                                     {
                                         matches = goodEnoughMatches.ToList();
